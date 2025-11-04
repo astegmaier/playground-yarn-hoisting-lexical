44cad9775035bca3715b7b68ce0a94f904038656
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2025-11-04 11:44:25 -0500

    Tweak actual imports in package a and b to import and log real things

--- packages/a/index.js
+++ packages/a/index.js
@@ -1,4 +1,5 @@
 // Importing this will import @lexical/clipboard as a side-effect. We've added console.log statements to all react packages that get installed, so you can see which one ends up being used.
 
 // Testing out an import that is actually declared in a/package.json
-// TODO: import something from @lexical/markdown and log it.
+import LexicalMarkdown from "@lexical/markdown";
+console.log(!!LexicalMarkdown);
--- packages/b/index.js
+++ packages/b/index.js
@@ -1,9 +1,3 @@
-// Importing this will import react as a side-effect. We've added console.log statements to all react packages that get installed, so you can see which one ends up being used.
-
 // Testing out an import that is actually declared in b/package.json
-import { safePolygon } from "@floating-ui/react";
-console.log(safePolygon);
-
-// Another test to see what happens if we import directly from @floating-ui/react-dom (which isn't directly declared in any package.json)
-// import { useFloating } from "@floating-ui/react-dom";
-// console.log(useFloating);
+import { setLexicalClipboardDataTransfer } from "@lexical/clipboard";
+console.log(setLexicalClipboardDataTransfer);
